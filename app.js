67ef6d7e40d9d5250d45e198d127f138a6a3dbb2
Adarsh Ranjan <adarshranjan4242@gmail.com>
2022-01-29 12:34:37 +0530

    All the files of daily journal is uploaded

---
+++ app.js
@@ -0,0 +1,181 @@
+//jshint esversion:6
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+mongoose.connect("mongodb://localhost:27017/notesDB", {useNewUrlParser: true});
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+//let posts = [];
+const PostSchema = new mongoose.Schema({
+  title : String,
+  content : String
+});
+const Post =mongoose.model("Post",PostSchema);
+
+//app.get("/", function(req, res){
+//  res.render("home", {
+///    startingContent: homeStartingContent,
+ //   posts : Post
+ //   });
+//});
+app.get("/", function(req, res){
+    Post.find({}, function(err, posts){
+    
+      res.render("home", {
+    
+        homeStartingContent: homeStartingContent,
+    
+        posts: posts
+    
+        });
+    
+    })
+  });
+
+
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+app.get("/update/:postId", function(req, res){
+  const requestedPostId = req.params.postId;
+  
+    Post.findOne({_id: requestedPostId}, function(err, post){
+      if(err){
+        console.log(err);
+      }
+      else{
+        //console.log(post.title);
+        //console.log(post.content);
+        //console.log(requestedPostId);
+      
+      res.render("update", {
+        title : post.title,
+        content : post.content,
+        id : requestedPostId
+    
+        });
+    
+      }
+    });
+});
+
+//app.get("/post", function(req, res){
+//  res.render("post");
+//});
+
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+
+  });
+
+  post.save(function(err){
+
+    if (!err){
+ 
+      res.redirect("/");
+ 
+    }
+ 
+  });
+
+});
+
+app.post("/update/:postId", function(req, res){
+  const requestedPostId = req.params.postId;
+  console.log(requestedPostId);
+  
+  Post.updateOne({_id: requestedPostId},{
+    title : req.body.uptitle,
+    content : req.body.upcontent
+  },function(err){
+    if(err){
+      console.log(err);
+    }
+    else
+    {
+      res.redirect("/");
+    }
+  });
+  
+});
+
+app.post("/delete/:postId", function(req, res){
+  const requestedPostId = req.params.postId;
+  
+  
+  Post.deleteOne({_id: requestedPostId},function(err){
+    if(err){
+      console.log(err);
+    }
+    else
+    {
+      res.redirect("/");
+    }
+  });
+  
+});
+
+
+app.get("/posts/:postId", function(req, res){
+
+  const requestedPostId = req.params.postId;
+  
+    Post.findOne({_id: requestedPostId}, function(err, post){
+      if(err){
+        console.log(err);
+      }
+      else{
+      
+      res.render("post", {
+    
+        //homeStartingContent: homeStartingContent,
+        title : post.title,
+        content : post.content,
+        id : requestedPostId
+    
+        });
+      //res.render("post", {
+      //  title: post.title,
+      //  content: post.content
+      //});
+    
+      }
+    });
+      
+  
+  });
+
+Post.find(function(err,posts){
+  if(err){
+    console.log(err);
+  }
+  else{
+    console.log(posts.title);
+  }
+})
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
